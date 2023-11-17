134f92926c78529b80ed9fb24eaf77064dc0b3a6
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-17 15:08:23 -0800

    Add bean data + methods for click event & new beans

--- src/components/CoffeeBeanControl.js
+++ src/components/CoffeeBeanControl.js
@@ -3,3 +3,24 @@ import NewCoffeeBeanForm from './NewCoffeeBeanForm';
 import CoffeeBeanList from './CoffeeBeanList';
 import CoffeeBeanDetail from './CoffeeBeanDetail';
 import EditCoffeeBeanForm from './EditCoffeeBeanForm';
+
+class CoffeeBeanControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      selectedCoffeeBean: null,
+      editing: false,
+      mainCoffeeBeanList: [
+        {
+          name: 'Blue Bottle Coffee Bright Home Blend',
+          origin: 'East Africa',
+          roast: 'Light Roast',
+          price: 15.99,
+          availability: 130
+        }
+      ]
+    }
+  }
+}
