ff4c3e6d4624cf0057220c51591bf9ab0e810c4d
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-17 14:52:38 -0800

    Create CoffeeBeanControl component + add imports

---
+++ src/components/CoffeeBeanControl.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import NewCoffeeBeanForm from './NewCoffeeBeanForm';
+import CoffeeBeanList from './CoffeeBeanList';
+import CoffeeBeanDetail from './CoffeeBeanDetail';
+import EditCoffeeBeanForm from './EditCoffeeBeanForm';
