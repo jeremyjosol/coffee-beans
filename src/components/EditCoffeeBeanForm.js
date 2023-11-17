97d6dd7a5c24cfc60d8e49a9d944a74164e55386
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-17 14:49:31 -0800

    Create EditCoffeeBeanForm component + add propTypes

---
+++ src/components/EditCoffeeBeanForm.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+
+function EditCoffeeBeanForm(props) {
+  const { bean } = props;
+  
+  function handleEditCoffeeBeanFormSubmission(event) {
+    event.preventDefault();
+    
+    props.onEditCoffeeBean({
+      name: event.target.name.value,
+      origin: event.target.origin.value,
+      roast: event.target.roast.value,
+      price: event.target.price.value,
+      availablility: event.target.available.value,
+      id: bean.id
+    });
+  }
+  
+  return (  
+    <React.Fragment>
+      <ReusableForm 
+        formSubmissionHandler={handleEditCoffeeBeanFormSubmission} 
+        buttonText="Update Bean" />
+    </React.Fragment>
+  );
+}
+
+EditCoffeeBeanForm.propTypes = {
+  bean: PropTypes.object,
+  onEditCoffeeBean: PropTypes.func
+};
+
+export default EditCoffeeBeanForm;
