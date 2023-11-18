4656e702d08dbac6baff3382e9a965100871bb56
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-17 18:50:41 -0800

    Add onClick method to scroll down to order

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 function Header() {
+  const scrollToOrderSection = () => {
+    const orderSection = document.getElementById('orderSection');
+    if (orderSection) {
+      orderSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className='header'>
       <div className='columns'>
@@ -8,8 +15,8 @@ function Header() {
           <h1 className='hero'>INDULGE IN THE ART OF FRESHNESS</h1> 
           <div className='columnTwo'>
             <h1>EXCLUSIVE <span className='coffee-beans'>COFFEE BEANS</span> ROASTED IN-HOUSE FOR YOUR SPECIAL BREW.</h1>
-            <div className='order'>
-              <button>ORDER NOW</button>
+            <div id="orderSection" className='order'>
+              <button onClick={scrollToOrderSection}>ORDER NOW</button>
             </div>
           </div>
         </div>
