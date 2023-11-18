619719f584c0ec0c2d4a3b1e925c1ce1e24d52e6
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-18 14:42:47 -0800

    Clean up code :sponge: :soap: :sparkles:

--- src/components/CoffeeBeanDetail.js
+++ src/components/CoffeeBeanDetail.js
@@ -10,9 +10,9 @@ function CoffeeBeanDetail(props) {
   let editIcon = (
     <svg 
       stroke='currentColor' 
-      fill='currentColor"'
+      fill='currentColor'
       strokeWidth='0' 
-      viewBox='0 0 1024 1024"'
+      viewBox='0 0 1024 1024'
       height='1em'
       width='1em' 
       xmlns='http://www.w3.org/2000/svg'>
