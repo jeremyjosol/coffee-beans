1610ab1e214f0317a7f228455a6aff30e8a9e8a2
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-17 19:02:44 -0800

    Create Navbar component + import & add basic styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Navbar from "./Navbar";
 import Header from "./Header";
 import CoffeeBeanControl from "./CoffeeBeanControl";
 
 function App(){
   return ( 
     <React.Fragment>
+      <div className="navbar">
+        <Navbar />
+      </div>
       <div className="header">
         <Header />
       </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className='nav-links'>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#products">Products</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
+
