a0031a2a18051c9c243802bd34498c5414afbc2f
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-18 12:54:54 -0800

    Check if > 0 display 'In Stock'

--- src/components/CoffeeBean.js
+++ src/components/CoffeeBean.js
@@ -7,10 +7,10 @@ const CoffeeBean = (props) => {
     <React.Fragment>
       <div className='coffee-bean' onClick = {() => props.whenCoffeeBeanClicked(props.id)}>
         <h2 className='blue-bottle'><strong>{props.brand.toUpperCase()}</strong></h2>
-        <h3>{props.name} | <em>{props.origin}</em></h3> 
-        <h5>{props.roast}</h5>
+        <h3 className='name'>{props.name} | <em>{props.origin}</em></h3> 
+        <h5 className='roast'><strong>{props.roast.toUpperCase()}</strong></h5>
         <h5>${props.price} per lb</h5>
-        <h5>{props.availability} lbs available</h5>
+        <h5 className='availability'>{props.availability > 0 ? 'In Stock' : `${props.availability} lbs available`}</h5>
         <hr/>
       </div>
     </React.Fragment>
