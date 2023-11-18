1b6d9f7b9fb834784ea969e40b465268659a506d
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-18 13:06:19 -0800

    Edit styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,23 +3,23 @@ import logo from '.././img/blue-bottle.jpg';
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light">
+    <nav className="navbar navbar-expand-lg navbar-light">
       <a className="navbar-brand" href="#">
         <img src={logo} alt="logo" className='logo-image' />
       </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">HOME <span className="sr-only">(current)</span></a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">ABOUT</a>
+          <li className="nav-item">
+            <a className="nav-link" href="#">ABOUT</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">CONTACT</a>
+          <li className="nav-item">
+            <a className="nav-link" href="#">CONTACT</a>
           </li>
         </ul>
       </div>
